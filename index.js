12df9baf02d4c5eaffc0ec03bcbe7378bdd75097
Pratik Thapa <pthapa@alarm.com>
2022-06-11 15:46:15 -0600

    completed name and  address extraction

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
-// const { link } = require('fs');
 
-const query = 'hotels in Germany';
+const query = 'hotels in Canada';
 const url = `https://www.google.com/maps/search/${query}`;
 
 (async () => {
@@ -21,32 +20,41 @@ const url = `https://www.google.com/maps/search/${query}`;
       // separated in order to get two different arrays.
     )
   );
-  console.log(businessNames);
+  //console.log(businessNames); // generates array of names.
+
   // businessLinks
   const businessLinks = await page.$$eval('.hfpxzc', (divs) =>
     Array.from(divs).map((div) => div?.getAttribute('href'))
   );
-  // console.log(businessLinks);
 
-  // lets loop through above arrays and grab addresses.
+  // lets loop through above business link arrays and grab addresses.
+  const businessAddressArray = [];
+
   for (let businessLink of businessLinks) {
     await page.goto(businessLink);
     const selectBusinessAddress = await page.$('.Io6YTe');
+
     const businessAddress = await selectBusinessAddress.evaluate(
       (el) => el.textContent
     );
-
-    console.log(businessAddress);
-
-    // combine all the extracted values into an array.
-
-    // const businessAddressArray = [];
-
-    // for (let i = 0; i < businessAddress.length; i++) {
-    //   businessAddressArray.push(i);
-    // }
-    // console.log(businessAddressArray);
+    businessAddressArray.push(businessAddress);
   }
+  //console.log(businessAddressArray);
+
+  // make a JSON Object from the two arrays
+
+  const combinedBusinessInfo = (businessNames, businessAddressArray) => {
+    const combinedArray = [];
+    for (let i = 0; i < businessNames.length; i++) {
+      combinedArray.push({
+        name: businessNames[i],
+        address: businessAddressArray[i],
+      });
+    }
+    return combinedArray;
+  };
+
+  console.log(combinedBusinessInfo(businessNames, businessAddressArray));
 
   await browser.close();
 })();
