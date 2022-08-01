4a88e3085421e310a95293b9fb19a74173ec6a4c
Pratik Thapa <pratikthapa@Dark-Beauty-16.local>
2022-07-31 20:03:11 -0600

    performance improvements, scrolling for more items, and avoid memory leak

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const userAgent = require('user-agents');
 const express = require('express');
+const bluebird = require('bluebird');
 const port = 3000;
 
 // Error Messages
@@ -11,6 +12,61 @@ const specialCharacterErrorMessage = `Please enter country or city Name without 
 const emptyStringCheck = /^\s*$/;
 const specialCharacterCheck = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
+// async disposer pattern
+const withBrowser = async (fn) => {
+  const browser = await puppeteer.launch({
+    /* launch options here */
+  });
+  try {
+    return await fn(browser);
+  } finally {
+    await browser.close();
+  }
+};
+
+const withPage = (browser) => async (fn) => {
+  const page = await browser.newPage();
+  try {
+    return await fn(page);
+  } finally {
+    await page.close();
+  }
+};
+
+// scrolling function
+function extractItems() {
+  const extractedElements = document.querySelectorAll('.hfpxzc');
+  const items = [];
+  for (let element of extractedElements) {
+    items.push(element.getAttribute('aria-label'));
+  }
+  return items;
+}
+async function scrapeItems(page, extractItems, itemCount, scrollDelay = 2000) {
+  let items = [];
+  try {
+    let previousHeight;
+    while (items.length < itemCount) {
+      //console.log(`items.length: ${items.length} itemCount: ${itemCount}`);
+
+      items = await page.evaluate(extractItems);
+
+      previousHeight = await page.evaluate(() => {
+        const scroller = document.querySelector('div.DxyBCb:nth-child(1)');
+        return scroller.scrollHeight;
+      });
+
+      await page.evaluate(
+        `document.querySelector("div.DxyBCb:nth-child(1)").scrollTo(0, ${previousHeight})`
+      );
+      await page.waitForFunction(
+        `document.querySelector("div.DxyBCb:nth-child(1)").scrollHeight > ${previousHeight}`
+      );
+      await page.waitForTimeout(scrollDelay);
+    }
+  } catch (e) {}
+  return items;
+}
 const app = express();
 
 app.get('/address/:countryOrCityName', (req, res, next) => {
@@ -18,8 +74,7 @@ app.get('/address/:countryOrCityName', (req, res, next) => {
   const url = `https://www.google.com/maps/search/${query}`;
   const userInput = req.params.countryOrCityName;
 
-  // Check if paramater is empty and check for special chraracters used in the query.
-
+  // Check for invalid characters or empty string on user's input (params).
   if (emptyStringCheck.test(userInput) || !userInput) {
     res.status(400).json({ mesage: `⚠️ EMPTY STRING ⚠️. ${errorMessage}` });
   } else if (specialCharacterCheck.test(userInput)) {
@@ -34,20 +89,10 @@ app.get('/address/:countryOrCityName', (req, res, next) => {
       await page.setUserAgent(userAgent.toString());
       await page.goto(url);
 
-      // scroll and add more content.
+      // auto-scroll and extract business Names
+      const businessNames = await scrapeItems(page, extractItems, 20);
 
-      // getting Names of business
-      await page.waitForSelector('.hfpxzc');
-      const businessNames = await page.$$eval('.hfpxzc', (divs) =>
-        Array.from(divs).map(
-          (div) => div?.getAttribute('aria-label')
-          //&& div?.getAttribute('href')
-          // separated in order to get two different arrays.
-        )
-      );
-      //console.log(businessNames); // generates array of names.
-
-      // businessLinks
+      // extract businessLinks
       const businessLinks = await page.$$eval('.hfpxzc', (divs) =>
         Array.from(divs).map((div) => div?.getAttribute('href'))
       );
@@ -55,15 +100,22 @@ app.get('/address/:countryOrCityName', (req, res, next) => {
       // lets loop through above business link arrays and grab addresses.
       const businessAddressArray = [];
 
-      for (let businessLink of businessLinks) {
-        await page.goto(businessLink);
-        const selectBusinessAddress = await page.$('.Io6YTe');
-
-        const businessAddress = await selectBusinessAddress.evaluate(
-          (el) => el.textContent
+      await withBrowser(async (browser) => {
+        return bluebird.map(
+          businessLinks,
+          async (businessLink) => {
+            return withPage(browser)(async (page) => {
+              await page.goto(businessLink);
+              const selectBusinessAddress = await page.$('.Io6YTe');
+              const businessAddress = await selectBusinessAddress.evaluate(
+                (el) => el.textContent
+              );
+              businessAddressArray.push(businessAddress);
+            });
+          },
+          { concurrency: 10 }
         );
-        businessAddressArray.push(businessAddress);
-      }
+      });
       //console.log(businessAddressArray);
 
       // make a JSON Object from the two arrays
