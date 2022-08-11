0d8001c50e56e6bb07bc4e9c38afa9d2e97c871f
Pratik Thapa <pratikthapa@Dark-Beauty-16.local>
2022-08-10 19:44:18 -0600

    reversing deployment changes

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.get('/address/:countryOrCityName', (req, res) => {
   } else {
     (async () => {
       const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome-stable',
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
       });
       const page = await browser.newPage();
       await page.setUserAgent(userAgent.toString());
